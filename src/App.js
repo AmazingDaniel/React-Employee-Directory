24d8a2b4d64a54f0548b0a1b8168ff2c261b4bfd
Daniel Moreno <drmjr417@sbcglobal.net>
2020-11-23 20:41:31 -0600

    clean up code add env.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Table from "./components/Table";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import empolyees from "./employees.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // Setting this.state.empolyees to the empolyees json array
+  state = {
+    empolyees
+  };
+
+  removeEmployee = id => {
+    // Filter this.state.empolyees for empolyees with an id not equal to the id being removed
+    const empolyees = this.state.empolyees.filter(empolyee => empolyee.id !== id);
+    // Set this.state.empolyees equal to the new empolyees array
+    this.setState({ empolyees });
+  };
+
+  // Map over this.state.empolyees and render a EmployeeCard component for each empolyee object
+  render() {
+    return (
+      <Wrapper>
+        <Title>Empolyees List</Title>
+        <Table/>
+        {/* {this.state.empolyees.map(empolyee => (
+          <EmployeeCard
+            removeEmployee={this.removeEmployee}
+            id={empolyee.id}
+            key={empolyee.id}
+            name={empolyee.name}
+            image={empolyee.image}
+            occupation={empolyee.occupation}
+            location={empolyee.location}
+            salary={employee.salary}
+            manager={employee.manager}
+          />
+        ))} */}
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
